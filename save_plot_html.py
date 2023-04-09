2d20edf57848a791b6b7c50db174803eaac5eaaf
Hossein Zolfaghari <96697972+Hosseinzlf@users.noreply.github.com>
2023-04-09 13:35:39 +0200

    Update save_plot_html.py

--- save_plot_html.py
+++ save_plot_html.py
@@ -11,15 +11,15 @@ fig = go.Figure()
 #plot number 1 with line mode, name title of plot, x , and y.
 fig.add_trace(go.Scatter(x=list(range(len(y_pred))), y= inputs_sc[:,2],
                     mode='lines',
-                    name='Measured GHI Data'))
+                    name='Measured Data'))
 #plot number 2                    
 fig.add_trace(go.Scatter(x=list(range(len(y_pred))), y= y_pred[:,2],
                     mode='lines',
-                    name='Predicted GHI data'))
+                    name='Predicted data'))
 #Adding xlabel, ylabel, title
 fig.update_layout(title='Model dropout',
-                   xaxis_title='GHI index',
-                   yaxis_title='GHI Normalized Value')
+                   xaxis_title='data index',
+                   yaxis_title='data Normalized Value')
 fig.show()
 #Give address and the name of the file to save as html
 plotly.io.write_html(fig, "/content/output.html")

1bbaf38125f50eaf5b9a7dfe9c1abae865f6b824
Hossein Zolfaghari <96697972+Hosseinzlf@users.noreply.github.com>
2023-04-13 15:33:40 +0200

    Add files via upload

---
+++ download_python_code.py
@@ -0,0 +1,18 @@
+#The code is developed by Hossein ZOLFAGHARI
+# April 2023
+
+# Get python file from a path and use it
+
+import requests
+from pathlib import Path 
+
+# Download helper functions from Learn PyTorch repo (if not already downloaded)
+if Path("helper_functions.py").is_file():
+  print("helper_functions.py already exists, skipping download")
+else:
+  print("Downloading helper_functions.py")
+  request = requests.get("https://raw.githubusercontent.com/mrdbourke/pytorch-deep-learning/main/helper_functions.py")
+  with open("helper_functions.py", "wb") as f:
+    f.write(request.content)
+
+from helper_functions import plot_predictions, plot_decision_boundary
